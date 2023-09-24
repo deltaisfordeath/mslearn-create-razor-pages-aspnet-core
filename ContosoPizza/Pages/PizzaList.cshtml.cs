123509b10c129bceed4441c1bdd23cdacb052e00
Kenny Heagle <kennyheagle@gmail.com>
2023-09-24 06:31:40 -0700

    added new pizza form with validation

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -15,9 +15,23 @@ namespace ContosoPizza.Pages
             _service = service;
         }
 
+        [BindProperty]
+        public Pizza NewPizza { get;set; } = default!;
+
         public void OnGet()
         {
             PizzaList = _service.GetPizzas();
+        }
+
+         public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || NewPizza == null)
+            {
+                return Page();
+            }
+
+            _service.AddPizza(NewPizza);
+            return RedirectToAction("Get");
         }
     }
 }
